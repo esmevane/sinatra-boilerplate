52819d4ca38eb2e829424f31dfa16e8e4092815e
Joseph McCormick <esmevane@gmail.com>
2012-05-03 16:21:04 -0500

    Incorporated coffee-script.

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source "http://rubygems.org"
 gem 'sinatra'
 gem 'haml'
 gem 'bson_ext'
+gem 'coffee-script'
 gem 'mongoid'
--- app.rb
+++ app.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'mongoid'
 require 'haml'
+require 'coffee-script'
 
 set :views, [ File.dirname(__FILE__), 'views' ].join('/')
 
 get '/' do
   haml :home
+end
+
+get '/scripts/lib/:script' do
+  content_type 'text/javascript'
+
+  coffee params[:script].to_sym
 end
